9e23f837451e1400a94cbe584e90d29f12a54bc6
Tarak Blah <halbkarat@gmail.com>
2017-06-05 16:27:44 +0200

    .First release.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-hello-world',
-    version='0.1',
+    version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
